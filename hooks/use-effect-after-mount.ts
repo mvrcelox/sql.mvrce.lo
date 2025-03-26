72d40096a2e1e6ecd1c85370cdfa07b3d8cd17c3
Lorenzo <https.braganca@gmail.com>
2025-03-26 08:19:07 -0300

    fix: disable warning of exhaustive deps

--- hooks/use-effect-after-mount.ts
+++ hooks/use-effect-after-mount.ts
@@ -10,5 +10,6 @@ export default function useEffectAfterMount(fn: () => void, deps: unknown[] = []
       }
 
       fn();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
    }, deps);
 }

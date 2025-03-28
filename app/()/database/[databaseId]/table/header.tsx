4b65a78528c00775618c9ed6eb4304cf6d33fb34
Lorenzo <https.braganca@gmail.com>
2025-03-28 08:25:49 -0300

    fix: improve ui on table header

--- app/()/database/[databaseId]/table/header.tsx
+++ app/()/database/[databaseId]/table/header.tsx
@@ -1,35 +1,88 @@
 "use client";
 
-import Button from "@/app/components/ui/button";
+import { buttonVariants } from "@/app/components/ui/button";
+import { AnimatePresence, LayoutGroup, motion } from "motion/react";
 import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function TableHeader() {
    const params = useParams();
    const pathname = usePathname();
+   const [hover, setHover] = useState<number | null>(null);
 
    const endWithProperties = pathname.endsWith("properties");
 
    return (
-      <div className="flex h-[calc(2.5rem+1px)] items-center border-b bg-gray-200 p-1">
-         <Button
-            asChild
-            aria-selected={endWithProperties}
-            intent={endWithProperties ? "outline" : "ghost"}
-            size="sm"
-            className={"border [&:not([aria-selected='true'])]:border-transparent"}
+      <LayoutGroup id="table-header">
+         <div
+            onMouseLeave={() => setHover(null)}
+            className="isolate flex h-[calc(2.5rem+1px)] items-center border-b bg-transparent p-1"
          >
-            <Link href={`/database/${params.databaseId}/table/${params.tableName}/properties`}>Properties</Link>
-         </Button>
-         <Button
-            asChild
-            aria-selected={!endWithProperties}
-            intent={endWithProperties ? "ghost" : "outline"}
-            size="sm"
-            className={"border [&:not([aria-selected='true'])]:border-transparent"}
-         >
-            <Link href={`/database/${params.databaseId}/table/${params.tableName}`}>Data</Link>
-         </Button>
-      </div>
+            <Link
+               aria-selected={endWithProperties}
+               onMouseEnter={() => setHover(0)}
+               className={buttonVariants({
+                  intent: "none",
+                  size: "sm",
+                  className: "hover:text-foreground group relative text-gray-600 aria-selected:text-gray-800",
+               })}
+               href={`/database/${params.databaseId}/table/${params.tableName}/properties`}
+            >
+               <AnimatePresence>
+                  {hover === 0 ? (
+                     <motion.span
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        layoutId="hover"
+                        className="bg-background absolute inset-0 -z-10 size-full rounded-[inherit] border"
+                        transition={{ type: "spring", duration: 0.2, bounce: 0.1 }}
+                     />
+                  ) : null}
+               </AnimatePresence>
+               {endWithProperties ? (
+                  <motion.span
+                     layoutId="selector"
+                     className="bg-primary absolute inset-x-0 -bottom-1 z-10 size-full h-0.5 w-full"
+                     transition={{ type: "spring", duration: 0.25, bounce: 0.2 }}
+                  />
+               ) : null}
+               Properties
+            </Link>
+
+            <Link
+               aria-selected={!endWithProperties}
+               onMouseEnter={() => setHover(1)}
+               className={buttonVariants({
+                  intent: "none",
+                  size: "sm",
+                  className: "hover:text-foreground group relative text-gray-600 aria-selected:text-gray-800",
+               })}
+               href={`/database/${params.databaseId}/table/${params.tableName}`}
+            >
+               <AnimatePresence>
+                  {hover === 1 ? (
+                     <motion.span
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                        layoutId="hover"
+                        className="bg-background absolute inset-0 -z-10 size-full rounded-[inherit] border"
+                        transition={{ type: "spring", duration: 0.2, bounce: 0.1 }}
+                     />
+                  ) : null}
+               </AnimatePresence>
+               {!endWithProperties ? (
+                  <motion.span
+                     layoutId="selector"
+                     className="bg-primary absolute inset-x-0 -bottom-1 z-10 size-full h-0.5 w-full"
+                     transition={{ type: "spring", duration: 0.25, bounce: 0.2 }}
+                  />
+               ) : null}
+               Data
+            </Link>
+         </div>
+      </LayoutGroup>
    );
 }

f5e7b57187989946bf6e5ce14171e7bb792a81b7
mvrcelitos <https.braganca@gmail.com>
2025-04-04 09:16:43 -0300

    feat: change ui and implement get table on psql database

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -6,20 +6,20 @@ import { cn } from "@/lib/utils";
 
 type ButtonVariants = VariantProps<typeof buttonVariants>;
 const buttonVariants = cva(
-   "inline-flex items-center rounded-md justify-center whitespace-nowrap text-sm font-medium cursor-pointer outline-hidden outline-offset-1 focus-visible:outline-2 transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0",
+   "inline-flex items-center rounded-md justify-center whitespace-nowrap text-sm font-medium cursor-pointer outline-hidden focus-visible:outline-2 transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0",
    {
       variants: {
          intent: {
             none: null,
             default: "bg-gray-900 text-background hover:bg-gray-900/90",
-            primary: "bg-primary hover:bg-primary-hover text-neutral-50 outline-primary/60",
+            primary: "bg-primary hover:bg-primary-hover text-neutral-50 outline-foreground",
             destructive:
                "bg-red-600 text-gray-50 hover:bg-rose-700 dark:bg-red-600 dark:hover:bg-red-500 outline-red-500",
             outline:
-               "bg-background border border-border text-gray-700 hover:border-gray-400 hover:bg-gray-100 hover:text-foreground outline-border",
+               "bg-background focus-visible:!border-transparent border border-border text-gray-700 hover:border-gray-400 hover:bg-gray-100 hover:text-foreground outline-foreground",
             secondary:
                "bg-gray-100 text-gray-900 hover:bg-gray-100/80 dark:bg-gray-800 dark:text-gray-50 dark:hover:bg-gray-800/80",
-            ghost: "text-gray-700 hover:bg-gray-200 hover:text-foreground outline-transparent focus-visible:bg-gray-200",
+            ghost: "text-gray-700 hover:bg-gray-200 hover:text-foreground outline-foreground focus-visible:bg-gray-200",
             link: "text-gray-700 underline-offset-2 hover:text-foreground",
          },
          size: {
--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -24,7 +24,7 @@ export const inputVariants = cva(
          },
       },
       defaultVariants: {
-         intent: "primary",
+         intent: "gray",
          size: "md",
       },
    },
